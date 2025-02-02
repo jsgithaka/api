6d3d2f108c3189ee49b526d81c0e13fe47feda2a
jsgithaka <jsgithaka@gmail.com>
2025-02-02 17:20:35 +0300

    feat(exports/styles): export `Input` component's style module

--- src/exports/styles/shared.ts
+++ src/exports/styles/shared.ts
@@ -1,2 +1,3 @@
 export { default as NavigationItemStyles } from '@/styles/shared/NavigationItem/NavigationItem.module.css';
 export { default as FormStyles } from '@/styles/shared/Form/Form.module.css';
+export { default as InputStyles } from '@/styles/shared/Input/Input.module.css';
