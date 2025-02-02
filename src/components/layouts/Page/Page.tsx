3dfe1ed857b3ea52943a5ac6637bc89078220e7e
jsgithaka <jsgithaka@gmail.com>
2025-02-02 14:36:30 +0300

    feat(components/layouts/Page): mount `Header` and `Main` components
    Also adjusted `Page` props.

--- src/components/layouts/Page/Page.tsx
+++ src/components/layouts/Page/Page.tsx
@@ -1,10 +1,17 @@
 import { PageStyles } from '@layout/styles';
+import { Header, Main } from '@segments/components';
 import { ReactNode } from 'react';
 
 interface Props {
-    children: ReactNode;
+    headerChildren: ReactNode;
+    mainChildren: ReactNode;
 }
 
-export default function Page({ children }: Props) {
-    return <body className={PageStyles.Page}>{children}</body>;
+export default function Page({ headerChildren, mainChildren }: Props) {
+    return (
+        <body className={PageStyles.Page}>
+            <Header />
+            <Main />
+        </body>
+    );
 }
