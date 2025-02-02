07889f437318bcd13b1cc017824b508be6ac6caf
jsgithaka <jsgithaka@gmail.com>
2025-02-02 16:12:01 +0300

    feat(app/page.tsx): update `headerChildren` prop content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
 import { Page } from '@/exports/components/layouts';
+import { NavigationItem } from '@/exports/components/shared';
 
 export default function Home() {
     return (
         <Page
-            headerChildren={<></>}
+            headerChildren={
+                <>
+                    <NavigationItem
+                        href='/'
+                        name='Home'
+                    />
+                    <NavigationItem
+                        href='/login'
+                        name='Login'
+                    />
+                    <NavigationItem
+                        href='/register'
+                        name='Register'
+                    />
+                    <NavigationItem
+                        href='/account'
+                        name='Account'
+                    />
+                    <NavigationItem
+                        href='/users'
+                        name='Users'
+                    />
+                </>
+            }
             mainChildren={<></>}
         />
     );
