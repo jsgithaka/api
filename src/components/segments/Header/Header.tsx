e283155e8fadcaeb88685a6904f7436321475e8d
jsgithaka <jsgithaka@gmail.com>
2025-02-02 14:38:45 +0300

    feat(components/segments/Header): add props and style module

--- src/components/segments/Header/Header.tsx
+++ src/components/segments/Header/Header.tsx
@@ -1,3 +1,10 @@
-export default function Header() {
-    return <header></header>;
+import { HeaderStyles } from '@segments/styles';
+import { ReactNode } from 'react';
+
+interface HeaderProps {
+    children: ReactNode;
+}
+
+export default function Header({ children }: HeaderProps) {
+    return <header className={HeaderStyles.Header}>{children}</header>;
 }
