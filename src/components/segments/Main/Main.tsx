6e0e9ded8146ef1ffb8699dc05451b6df35b02da
jsgithaka <jsgithaka@gmail.com>
2025-02-02 14:48:36 +0300

    feat(components/segments/Main): add props and mount styles

--- src/components/segments/Main/Main.tsx
+++ src/components/segments/Main/Main.tsx
@@ -1,3 +1,10 @@
-export default function Main() {
-    return <main></main>;
+import { MainStyles } from '@segments/styles';
+import { ReactNode } from 'react';
+
+interface MainProps {
+    children: ReactNode;
+}
+
+export default function Main({ children }: MainProps) {
+    return <main className={MainStyles.Main}>{children}</main>;
 }
