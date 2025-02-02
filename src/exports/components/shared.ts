504c9d961fdbdb50e54d9851feb99af2d0c16706
jsgithaka <jsgithaka@gmail.com>
2025-02-02 17:19:36 +0300

    feat(exports/component): export `Input` component

--- src/exports/components/shared.ts
+++ src/exports/components/shared.ts
@@ -1,2 +1,3 @@
 export { default as NavigationItem } from '@/components/shared/NavigationItem/NavigationItem';
 export { default as Form } from '@/components/shared/Form/Form';
+export { default as Input } from '@/components/shared/Input/Input';
