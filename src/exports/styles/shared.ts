e15503ab327d89bbfb7703acbd55cfbe80007419
jsgithaka <jsgithaka@gmail.com>
2025-02-02 16:54:40 +0300

    feat(exports/styles): export `Form` component's style module

--- src/exports/styles/shared.ts
+++ src/exports/styles/shared.ts
@@ -1,1 +1,2 @@
 export { default as NavigationItemStyles } from '@/styles/shared/NavigationItem/NavigationItem.module.css';
+export { default as FormStyles } from '@/styles/shared/Form/Form.module.css';
