8d625e20338d798b29df91aefeeed3ace2156228
jsgithaka <jsgithaka@gmail.com>
2025-02-02 17:28:27 +0300

    feat(exports/components): export the `View` component

--- src/exports/components/segments.ts
+++ src/exports/components/segments.ts
@@ -1,3 +1,4 @@
 export { default as Header } from '@/components/segments/Header/Header';
 export { default as Main } from '@/components/segments/Main/Main';
 export { default as Footer } from '@/components/segments/Footer/Footer';
+export { default as View } from '@/components/segments/View/View';
