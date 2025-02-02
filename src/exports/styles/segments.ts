2b9d3c0519d89cf98528bee1ef88a4aaa6fd1d27
jsgithaka <jsgithaka@gmail.com>
2025-02-02 15:25:43 +0300

    feat(exports/styles): export `Footer` style module

--- src/exports/styles/segments.ts
+++ src/exports/styles/segments.ts
@@ -1,2 +1,3 @@
 export { default as HeaderStyles } from '@/styles/segments/Header/Header.module.css';
 export { default as MainStyles } from '@/styles/segments/Main/Main.module.css';
+export { default as FooterStyles } from '@/styles/segments/Footer/Footer.module.css';
