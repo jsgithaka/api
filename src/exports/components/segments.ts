baa1c37546ad67c5914aee64c668ac22dfa5abb4
jsgithaka <jsgithaka@gmail.com>
2025-02-02 15:23:54 +0300

    feat(exports/components): export `Footer` component

--- src/exports/components/segments.ts
+++ src/exports/components/segments.ts
@@ -1,2 +1,3 @@
 export { default as Header } from '@/components/segments/Header/Header';
 export { default as Main } from '@/components/segments/Main/Main';
+export { default as Footer } from '@/components/segments/Footer/Footer';
