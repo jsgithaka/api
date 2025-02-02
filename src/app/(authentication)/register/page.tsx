6c01ee7b028c25a48deb4175ba640cb58f99c998
jsgithaka <jsgithaka@gmail.com>
2025-02-02 16:41:40 +0300

    feat(app/(authentication)): create `/register` route

---
+++ src/app/(authentication)/register/page.tsx
@@ -0,0 +1,26 @@
+import { Page } from '@/exports/components/layouts';
+import { NavigationItem } from '@/exports/components/shared';
+
+export default function Register() {
+    return (
+        <Page
+            headerChildren={
+                <>
+                    <NavigationItem
+                        href='/'
+                        name='Home'
+                    />
+                    <NavigationItem
+                        href='/login'
+                        name='Login'
+                    />
+                    <NavigationItem
+                        href='/register'
+                        name='Register'
+                    />
+                </>
+            }
+            mainChildren={<></>}
+        />
+    );
+}
