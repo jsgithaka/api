0c2424c4854474a804b55f49bb20a7f524ffb8d0
jsgithaka <jsgithaka@gmail.com>
2025-02-03 12:42:08 +0300

    fix(api/register): update the handler with correct response status codes

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -5,25 +5,35 @@ export async function POST(request: NextRequest) {
         const { email, password, confirm_password } = await request.json();
         const apiKey = request.headers.get('x-api-key');
         if (!apiKey || apiKey !== '1234') {
-            return NextResponse.json({ api: 'Unauthorized' });
+            return NextResponse.json({ api: 'Unauthorized' }, { status: 401 });
         } else {
             if (!email || !password || !confirm_password) {
-                return NextResponse.json({
-                    api: 'Some information is missing!',
-                });
+                return NextResponse.json(
+                    {
+                        api: 'Some information is missing!',
+                    },
+                    { status: 400 }
+                );
             } else if (password !== confirm_password) {
-                return NextResponse.json({
-                    api: 'Ensure password and confirm_password match!',
-                });
+                return NextResponse.json(
+                    {
+                        api: 'Ensure password and confirm_password match!',
+                    },
+                    { status: 400 }
+                );
             } else if (password.length < 8 || confirm_password.length < 8) {
-                return NextResponse.json({
-                    api: 'Kindly ensure your password is atleast 8 characters long!',
-                });
+                return NextResponse.json(
+                    {
+                        api: 'Kindly ensure your password is atleast 8 characters long!',
+                    },
+                    { status: 400 }
+                );
             } else {
-                return NextResponse.json({ api: 'Okay!' });
+                return NextResponse.json({ api: 'Okay!' }, { status: 201 });
+                // create a user in a postgres database - note the postgres database is installed locally not vercel stuff
             }
         }
     } catch (error) {
-        return NextResponse.json({ api: 'Not Okay!' });
+        return NextResponse.json({ api: 'Not Okay!' }, { status: 500 });
     }
 }
