826161aa884d180e44877ee639f5a3fc022b6af8
jsgithaka <jsgithaka@gmail.com>
2025-02-03 12:56:28 +0300

    feat(lib): secure credentials

--- src/lib/postgres-local.ts
+++ src/lib/postgres-local.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    user: 'your_postgres_username',
-    host: 'localhost',
-    database: 'your_database_name',
-    password: 'your_postgres_password',
-    port: 5432,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: Number(process.env.POSTGRES_PORT),
 });
 
 export default pool;
