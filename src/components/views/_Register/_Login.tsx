b9c11f9ec0ebf55c767797b9e5757a38896c42f5
jsgithaka <jsgithaka@gmail.com>
2025-02-03 08:54:42 +0300

    feat(components/views): create `_Register` component

---
+++ src/components/views/_Register/_Login.tsx
@@ -0,0 +1,33 @@
+import { View } from '@segments/components';
+import { Form, Input } from '@shared/components';
+
+export default function _Register() {
+    return (
+        <View id='register'>
+            <Form>
+                <Input
+                    type='email'
+                    placeholder='your@email.com'
+                    label='Email'
+                    id='email'
+                />
+                <Input
+                    type='password'
+                    placeholder='********'
+                    label='Password'
+                    id='password'
+                />
+                <Input
+                    type='password'
+                    placeholder='********'
+                    label='Password'
+                    id='confirm_password'
+                />
+                <Input
+                    type='submit'
+                    value='Register'
+                />
+            </Form>
+        </View>
+    );
+}
