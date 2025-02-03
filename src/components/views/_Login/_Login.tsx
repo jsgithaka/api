c89a13e215245c8530bcc489968872472d114526
jsgithaka <jsgithaka@gmail.com>
2025-02-03 08:52:22 +0300

    feat(components/views/_Login): mount `Input` components

--- src/components/views/_Login/_Login.tsx
+++ src/components/views/_Login/_Login.tsx
@@ -1,10 +1,27 @@
 import { View } from '@segments/components';
-import { Form } from '@shared/components';
+import { Form, Input } from '@shared/components';
 
 export default function _Login() {
     return (
         <View id='login'>
-            <Form />
+            <Form>
+                <Input
+                    type='email'
+                    placeholder='your@email.com'
+                    label='Email'
+                    id='email'
+                />
+                <Input
+                    type='password'
+                    placeholder='********'
+                    label='Password'
+                    id='password'
+                />
+                <Input
+                    type='submit'
+                    value='Login'
+                />
+            </Form>
         </View>
     );
 }
