64f05a08300b47d08a48e69b7fa00733a4e96e65
jsgithaka <jsgithaka@gmail.com>
2025-02-03 09:57:51 +0300

    feat(components/shared/Form): update component with function prop

--- src/components/shared/Form/Form.tsx
+++ src/components/shared/Form/Form.tsx
@@ -2,8 +2,9 @@ import { ReactNode } from 'react';
 
 interface FormProps {
     children: ReactNode;
+    method: () => void;
 }
 
-export default function Form({ children }: FormProps) {
-    return <form>{children}</form>;
+export default function Form({ children, method }: FormProps) {
+    return <form onSubmit={method}>{children}</form>;
 }
