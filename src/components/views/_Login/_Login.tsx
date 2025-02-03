3e1b8a43e17296856b8f75ef2fbd789b85c3897a
jsgithaka <jsgithaka@gmail.com>
2025-02-03 08:42:32 +0300

    feat(components/views/_Login): mount `Form` component

--- src/components/views/_Login/_Login.tsx
+++ src/components/views/_Login/_Login.tsx
@@ -1,9 +1,10 @@
 import { View } from '@segments/components';
+import { Form } from '@shared/components';
 
 export default function _Login() {
     return (
         <View id='login'>
-            <></>
+            <Form />
         </View>
     );
 }
